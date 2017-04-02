dafa017159570ec7f35aa0ee6f0a8b114c9094b6
rjm96 <rjm96@bu.edu>
2017-04-01 22:22:15 -0400

    Added extra streams_to_streams_ff test

--- python/qa_streams_to_streams_ff.py
+++ python/qa_streams_to_streams_ff.py
@@ -47,7 +47,34 @@ class qa_streams_to_streams_ff (gr_unittest.TestCase):
         print result_data
         self.assertFloatTuplesAlmostEqual(expected_results, result_data, 6)
         
+    def test_002_t (self):
+        #This tests multiple streams, to make sure it outputs the same input from before
+        src_data0=(1.0,2.0,3.0)
+        src_data1=(4.0,5.0,6.0)
+        src_data2=(7.0,8.0,9.0)
+        expected_result0=(1.0,2.0,3.0)
+        src0 = blocks.vector_source_f(src_data0)
+        src1 = blocks.vector_source_f(src_data1)
+        src2 = blocks.vector_source_f(src_data2)
+        blk = testblocks.streams_to_streams_ff()
+        dst0 = blocks.vector_sink_f()
+        dst1 = blocks.vector_sink_f()
+        dst2 = blocks.vector_sink_f()
+
+        self.tb.connect(src0, (blk,0) )
+        self.tb.connect(src1, (blk,1) )
+        self.tb.connect(src2, (blk,2) )                
+        self.tb.connect((blk,0), dst0)
+        self.tb.connect((blk,1), dst1)
+        self.tb.connect((blk,2), dst2)
         
+        self.tb.run()
+        result_data0 = dst0.data()
+        result_data1 = dst1.data()
+        result_data2 = dst2.data()
+        # check data
+        print result_data0
+        self.assertFloatTuplesAlmostEqual(expected_result0, result_data0, 6)
  
     
 
